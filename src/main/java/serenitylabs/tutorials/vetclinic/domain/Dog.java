71c081414d26382c68d775203658be550001f06a
Jeff Capanan <jeffcapanan@gmail.com>
2021-02-18 22:26:53 +1300

    test dog builder

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -1,2 +1,63 @@
-package serenitylabs.tutorials.vetclinic.domain;public class Dog {
+package serenitylabs.tutorials.vetclinic.domain;
+
+import java.security.cert.CertPathBuilder;
+import java.time.LocalDateTime;
+
+public class Dog {
+    private final String name;
+    private final String breed;
+    private final LocalDateTime dateOfBirth;
+    private String favouriteFood;
+
+    public Dog(String name, String breed, LocalDateTime dateOfBirth) {
+        this.name = name;
+        this.breed = breed;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Dog(String name, String breed, LocalDateTime dateOfBirth, String favouriteFood) {
+        this.name = name;
+        this.breed = breed;
+        this.dateOfBirth = dateOfBirth;
+        this.favouriteFood = favouriteFood;
+    }
+
+    public static DogBuilder called(String name) {
+        return new DogBuilder(name);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getBreed() {
+        return this.breed;
+    }
+
+    public LocalDateTime getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+
+    public static class DogBuilder {
+        private String name;
+        private String breed;
+        private LocalDateTime dateOfBirth;
+
+        public DogBuilder(String name) {
+            this.name = name;
+        }
+
+        public DogBuilder ofBreed(String breed) {
+            this.breed = breed;
+            return this;
+        }
+
+        public Dog bornOn(LocalDateTime dateOfBirth) {
+            return new Dog(name, breed, dateOfBirth);
+        }
+    }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCreatingANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCreatingANewDog.java
@@ -1,2 +1,31 @@
-package serenitylabs.tutorials.vetclinic.domain;public class WhenCreatingANewDog {
+package serenitylabs.tutorials.vetclinic.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+public class WhenCreatingANewDog {
+
+    @Test
+    public void it_should_have_a_name() throws Exception {
+        LocalDateTime birthday = LocalDateTime.now();
+
+        Dog fido = Dog.called("Fido")
+                      .ofBreed("Poodle")
+                      .bornOn(birthday);
+
+        Assert.assertEquals("Fido", fido.getName());
+        Assert.assertEquals("Poodle", fido.getBreed());
+        Assert.assertEquals(birthday, fido.getDateOfBirth());
+    }
+
+    @Test
+    public void it_should_have_an_optional_favourite_food() throws Exception {
+        LocalDateTime birthday = LocalDateTime.now();
+
+        Dog fido = new Dog("Fido", "Poodle", birthday, "pizza");
+
+        Assert.assertEquals("pizza", fido.getFavouriteFood());
+    }
 }
